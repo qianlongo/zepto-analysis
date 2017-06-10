fddbcf70183ffbcf1f9fb5698e0706de06f8b491
wangmin <1162633511@qq.com>
2017-06-10 16:44:48 +0800

    update: ajax jsonp请求原理分析完成

--- src/ajax.js
+++ src/ajax.js
@@ -91,36 +91,50 @@
   // Empty function, used as default callback
   function empty() { }
 
-  $.ajaxJSONP = function (options, deferred) {
-    if (!('type' in options)) return $.ajax(options)
+  // jsonp请求处理
 
+  $.ajaxJSONP = function (options, deferred) {
+    // 直接调ajaxJSONP没有传入type，去走$.ajax
+    if (!('type' in options)) return $.ajax(options)
+    // 获取callback函数名，此时未指定为undefined
     var _callbackName = options.jsonpCallback,
+      // jsonpCallback可以是一个函数或者一个字符串
+      // 是函数时，执行该函数拿到其返回值作为callback函数
+      // 为字符串时直接赋值
+      // 没有传入jsonpCallback，那么使用类似'Zepto3726472347'作为函数名
       callbackName = ($.isFunction(_callbackName) ?
         _callbackName() : _callbackName) || ('Zepto' + (jsonpID++)),
+      // 创建一个script标签用来发送请求  
       script = document.createElement('script'),
+      // 先读取全局的callbackName函数，因为后面会对该函数重写，所以需要先保存一份
       originalCallback = window[callbackName],
       responseData,
+      // 中止请求，触发script元素上的error事件, 后面带的参数是回调函数接收的参数
       abort = function (errorType) {
         $(script).triggerHandler('error', errorType || 'abort')
       },
       xhr = { abort: abort }, abortTimeout
 
     if (deferred) deferred.promise(xhr)
-
+    // 给script元素添加load和error事件
     $(script).on('load error', function (e, errorType) {
+      // 清除超时定时器
       clearTimeout(abortTimeout)
+      // 移除添加的元素(注意这里还off了，不然超时这种情况，请求回来了，还是会走回调)
       $(script).off().remove()
-
+      // 请求出错或后端没有给callback中塞入数据，将触发error
       if (e.type == 'error' || !responseData) {
         ajaxError(null, errorType || 'error', xhr, options, deferred)
       } else {
+        // 请求成功，调用成功回调，请塞入数据responseData[0]
         ajaxSuccess(responseData[0], xhr, options, deferred)
       }
-
+      // 将originalCallback重新赋值回去
       window[callbackName] = originalCallback
+      // 并且判断originalCallback是不是个函数，如果是函数，便执行
       if (responseData && $.isFunction(originalCallback))
         originalCallback(responseData[0])
-
+      // 清空闭包，释放空间
       originalCallback = responseData = undefined
     })
 
@@ -128,14 +142,15 @@
       abort('abort')
       return xhr
     }
-
+    // 重写全局上的callbackName
     window[callbackName] = function () {
       responseData = arguments
     }
-
+    // 将重写后的回调函数名追加到?后面
     script.src = options.url.replace(/\?(.+)=\?/, '?$1=' + callbackName)
+    // 添加script元素
     document.head.appendChild(script)
-
+    // 超时处理函数
     if (options.timeout > 0) abortTimeout = setTimeout(function () {
       abort('timeout')
     }, options.timeout)
@@ -144,25 +159,33 @@
   }
 
   $.ajaxSettings = {
+    // 请求类型
     // Default type of request
     type: 'GET',
+    // 请求发送前执行的函数
     // Callback that is executed before request
     beforeSend: empty,
+    // 请求成功后的回调
     // Callback that is executed if the request succeeds
     success: empty,
+    // 请求出错后的回调(具体有哪些出错愿意呢？)
     // Callback that is executed the the server drops error
     error: empty,
+    // 请求成功or失败都执行的回调
     // Callback that is executed on request complete (both: error and success)
     complete: empty,
     // The context for the callbacks
+    // 指定请求成功or失败等回调中的this指定，默认是window
     context: null,
     // 是否触发全局的ajax事件，默认是true
     // Whether to trigger "global" Ajax events
     global: true,
+    // 底层请求api
     // Transport
     xhr: function () {
       return new window.XMLHttpRequest()
     },
+    // 请求接受的数据类型
     // MIME types mapping
     // IIS returns Javascript as "application/x-javascript"
     accepts: {
@@ -176,11 +199,15 @@
     // Whether the request is to another domain
     crossDomain: false,
     // Default timeout
+    // 超时设置
     timeout: 0,
+    // 控制数据是否需要被序列化
     // Whether data should be serialized to string
     processData: true,
+    // 是否接受缓存请求(如何缓存？缓存哪些类型的请求？)
     // Whether the browser should be allowed to cache GET responses
     cache: true,
+    // 暂时还不知道干什么用
     //Used to handle the raw response data of XMLHttpRequest.
     //This is a pre-filtering function to sanitize the response.
     //The sanitized response should be returned
@@ -200,10 +227,16 @@
     return (url + '&' + query).replace(/[&?]{1,2}/, '?')
   }
 
+  // 序列化options中的data参数
+  // {name: 'qianlongo', sex: 'boy'} => name=qianlongo&sex=boy
+
   // serialize payload and append it to the URL for GET requests
   function serializeData(options) {
+    // 是否需要序列化 && data字段存在 && 不是字符串(为毛不直接判断是否为对象呢？)
     if (options.processData && options.data && $.type(options.data) != "string")
+      // 底层还是用的$.param进行的序列化
       options.data = $.param(options.data, options.traditional)
+    // get请求，jsonp请求将序列化后的data追加到url后
     if (options.data && (!options.type || options.type.toUpperCase() == 'GET' || 'jsonp' == options.dataType))
       options.url = appendQuery(options.url, options.data), options.data = undefined
   }
@@ -217,7 +250,7 @@
       // 如果引入了Deferred，将支持then,catch等方式触发回调
       deferred = $.Deferred && $.Deferred(),
       urlAnchor, hashIndex
-      // 将默认的参数合并到settings上(这里不直接使用$.extend(settings, $.ajaxSettings)估计是不想后者覆盖前者)
+    // 将默认的参数合并到settings上(这里不直接使用$.extend(settings, $.ajaxSettings)估计是不想后者覆盖前者)
     for (key in $.ajaxSettings) if (settings[key] === undefined) settings[key] = $.ajaxSettings[key]
     // 触发全局自定义事件ajaxStart => $(element).on('ajaxStart', () => {})
     ajaxStart(settings)
@@ -238,20 +271,25 @@
     // 2 去除url上的hash值
     if (!settings.url) settings.url = window.location.toString()
     if ((hashIndex = settings.url.indexOf('#')) > -1) settings.url = settings.url.slice(0, hashIndex)
-    // 参数序列化
+    // 参数序列化，所谓序列化也是讲settings中的data字段从对象变成字符串的形式
+    // {name: 'qianlongo', sex: 'boy'} => name=qianlongo&sex=boy
     serializeData(settings)
-
+    // 遇到请求地址类似 abc.com?a=xxx&b=xxx的时候手动设置预期返回数据类型为jsonp
     var dataType = settings.dataType, hasPlaceholder = /\?.+=\?/.test(settings.url)
     if (hasPlaceholder) dataType = 'jsonp'
-
+    // 不适用缓存，添加时间戳,以下情况不走缓存
+    // cache为false 
+    // options.cache不为true
+    // dataType为jsonp 或 script
     if (settings.cache === false || (
       (!options || options.cache !== true) &&
       ('script' == dataType || 'jsonp' == dataType)
     ))
       settings.url = appendQuery(settings.url, '_=' + Date.now())
-
+    // 当判断到是jsonp请求的时候，适用$.ajaxJSONP发送请求  
     if ('jsonp' == dataType) {
       if (!hasPlaceholder)
+        // 对是否提供了默认的回调函数名称进行处理，后端往该函数中塞入数据并返回
         settings.url = appendQuery(settings.url,
           settings.jsonp ? (settings.jsonp + '=?') : settings.jsonp === false ? '' : 'callback=?')
       return $.ajaxJSONP(settings, deferred)
