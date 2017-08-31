0411b041bca49075aff1b77c773c414bf74e0936
wangmin <1162633511@qq.com>
2017-08-31 16:45:30 +0800

    update: form.js模块分析

--- src/form.js
+++ src/form.js
@@ -9,21 +9,29 @@
         if (value.forEach) return value.forEach(add)
         result.push({ name: name, value: value })
       }
+    // this[0].elements => 如果选中多个表单，只序列化第一个表单
     if (this[0]) $.each(this[0].elements, function(_, field){
       type = field.type, name = field.name
+      // 排除掉fieldset,被禁止disable的元素,submit元素，以及reset,button，file和未被选中的radio，checkbox
       if (name && field.nodeName.toLowerCase() != 'fieldset' &&
         !field.disabled && type != 'submit' && type != 'reset' && type != 'button' && type != 'file' &&
         ((type != 'radio' && type != 'checkbox') || field.checked))
+          // 调用add方法，将表单元素的name和value以{ name: name, value: value }形式添加到result数组中
           add($(field).val())
     })
     return result
   }
 
+  // 将表单内容序列化为name=qianlongo&sex=boy的形式
+  // 主要还是依赖serializeArray函数
+
   $.fn.serialize = function(){
     var result = []
     this.serializeArray().forEach(function(elm){
+      // 每个表单的name和value都通过encodeURIComponent编码
       result.push(encodeURIComponent(elm.name) + '=' + encodeURIComponent(elm.value))
     })
+    // 最后通过&符号分割
     return result.join('&')
   }
 
