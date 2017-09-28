65bbbb67345713ec5ee13e8ac1097d6038172754
wangmin <1162633511@qq.com>
2017-09-28 23:24:37 +0800

    update: getComputedStyle文档解释

--- src/ie.js
+++ src/ie.js
@@ -2,6 +2,10 @@
 //     (c) 2010-2016 Thomas Fuchs
 //     Zepto.js may be freely distributed under the MIT license.
 
+/**
+ * getComputedStyle mdn文档 https://developer.mozilla.org/zh-CN/docs/Web/API/Window/getComputedStyle
+ */
+
 ;(function(){
   // getComputedStyle shouldn't freak out when called
   // without a valid element as argument
