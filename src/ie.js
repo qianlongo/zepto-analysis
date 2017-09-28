f1a570737b5fe61cea8a447faf122059e58fdd01
wangmin <1162633511@qq.com>
2017-09-28 23:34:21 +0800

    update: getComputedStyle分析完成

--- src/ie.js
+++ src/ie.js
@@ -10,6 +10,8 @@
   // getComputedStyle shouldn't freak out when called
   // without a valid element as argument
   // 重写getComputedStyle
+  // 第一个参数如果不是元素节点则会抛出错误，被catch捕获，并被重写。
+  // 重写后的方法，如果传入的第一个参数不是元素节点，被catch捕获，返回null，则不影响后续代码的运行
   try {
     getComputedStyle(undefined)
   } catch(e) {
