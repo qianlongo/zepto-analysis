fbd0e7cb596fe2197000f9751c20ba5fc2b1e5cc
wangmin23 <1162633511@qq.com>
2018-01-30 00:13:43 +0800

    add: assets模块解析

--- src/assets.js
+++ src/assets.js
@@ -1,9 +1,18 @@
 //     Zepto.js
 //     (c) 2010-2016 Thomas Fuchs
 //     Zepto.js may be freely distributed under the MIT license.
+// https://www.fngtps.com/2010/mobile-safari-image-resource-limit-workaround/
+// 为了解决 Safari 移动版加载图片过大过多时崩溃的问题而诞生的模块
+// https://segmentfault.com/q/1010000003285644 这是小A师兄在sg的回答 棒
 
 ;(function($){
   var cache = [], timeout
+
+  /**
+   *  1. 用较小的图片替代原图片
+   *  2. 将图片元素存入cache数组，便于后续垃圾回收器回收
+   *  3. 通过removeChild删除元素
+   */
 
   $.fn.remove = function(){
     return this.each(function(){
