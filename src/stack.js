f7d508baf6ae304d176c0fe1323149aa1d3a84b7
wangmin23 <1162633511@qq.com>
2018-02-06 21:46:50 +0800

    add: stack模块

--- src/stack.js
+++ src/stack.js
@@ -3,6 +3,8 @@
 //     Zepto.js may be freely distributed under the MIT license.
 
 ;(function($){
+  // 终止在当前链的最新过滤操作，并返回匹配的元素的以前状态。
+  // 可以参考https://www.jquery123.com/end/
   $.fn.end = function(){
     return this.prevObject || $()
   }
@@ -10,7 +12,7 @@
   $.fn.andSelf = function(){
     return this.add(this.prevObject || $())
   }
-
+  // 重写以下方法，并在返回结果值中存储prevObject属性，即原来状态的"实例"
   'filter,add,not,eq,first,last,find,closest,parents,parent,children,siblings'.split(',').forEach(function(property){
     var fn = $.fn[property]
     $.fn[property] = function(){
